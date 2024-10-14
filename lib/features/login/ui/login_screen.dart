a6e5e777fa8071295bc79c9058ef1eec861db471
mohamed bushra <m.bushra9577@gmail.com>
2024-10-14 12:12:08 +0400

    inital begin for the project

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('LoginScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('LoginScreen'),
+        ),
+        body: Center(
+          child: Text('LoginScreen is working'),
+        ));
   }
 }
--- lib/features/onBoarding/onBoarding_screen.dart
+++ lib/features/onBoarding/onBoarding_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+          ),
+        ),
+      ),
+    );
   }
 }
